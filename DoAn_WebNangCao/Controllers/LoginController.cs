c648293fbf561c4f4f194f77820eae66a526b20e
HungThai <thchannel1258vn@gmail.com>
2021-05-04 13:13:29 +0700

    Register UI 86%

--- DoAn_WebNangCao/Controllers/LoginController.cs
+++ DoAn_WebNangCao/Controllers/LoginController.cs
@@ -13,5 +13,17 @@ namespace DoAn_WebNangCao.Controllers
         {
             return View();
         }
+        public ActionResult LoginUser()
+        {
+            return View();
+        }
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+        public ActionResult RegisterUser1()
+        {
+            return View();
+        }
     }
 }
