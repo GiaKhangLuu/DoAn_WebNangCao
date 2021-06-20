a2d990a5f82c3881aebf8835e6fb83226973594c
thaihung2510 <thchannel1258vn@gmail.com>
2021-06-21 01:40:44 +0700

    test after bug :((

--- DoAn_WebNangCao/Models/ThiTracNghiemEntity.Designer.cs
+++ DoAn_WebNangCao/Models/ThiTracNghiemEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\GiaKhang\Desktop\DoAn_WebNangCao\DoAn_WebNangCao\Models\ThiTracNghiemEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\study\Webnangcao\Check\DoAn_WebNangCao\DoAn_WebNangCao\Models\ThiTracNghiemEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
