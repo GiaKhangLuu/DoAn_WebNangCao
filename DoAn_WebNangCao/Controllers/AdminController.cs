0f1a1b12cb9b03d14b43302f5e6f7759a04a9d72
thaihung2510 <thchannel1258vn@gmail.com>
2021-06-21 21:09:52 +0700

    Add UI

--- DoAn_WebNangCao/Controllers/AdminController.cs
+++ DoAn_WebNangCao/Controllers/AdminController.cs
@@ -11,6 +11,7 @@ namespace DoAn_WebNangCao.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+        THITRACNGHIEMEntities db = new THITRACNGHIEMEntities();
         public ActionResult Index()
         {
             if(Session["UserName"]==null)
@@ -19,6 +20,25 @@ namespace DoAn_WebNangCao.Controllers
             }
             ViewBag.Encrypt = Encryption.Encrypt("admin");
             return View();
+        }
+        public ActionResult Permission(string _name)
+        {
+            if (_name == null)
+                return View(db.TAIKHOANs.ToList());
+            else
+                return View(db.TAIKHOANs.Where(s => s.UserName.Contains(_name)).ToList());
+        }
+        [HttpGet]
+        public ActionResult CreateField()//Lĩnh vực
+        {
+            List<LINHVUC> linhvucLi = db.LINHVUCs.OrderBy(x => x.IDLinhVuc).ToList();
+            ViewData["linhvucLi"] = linhvucLi;
+            return View();
+        }
+        public ActionResult GetListLV()
+        {
+            var lvLi = db.LINHVUCs.ToList<LINHVUC>();
+            return Json(new { data = lvLi }, JsonRequestBehavior.AllowGet);
         }
     }
 }
