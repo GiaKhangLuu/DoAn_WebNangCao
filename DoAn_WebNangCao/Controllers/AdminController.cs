e8ebd901d8e9e00a70386af200674d469bef2140
Luu Gia Khang <waoknight@gmail.com>
2021-06-23 15:24:41 +0700

    add feature save fill in blank quiz

--- DoAn_WebNangCao/Controllers/AdminController.cs
+++ DoAn_WebNangCao/Controllers/AdminController.cs
@@ -82,7 +82,7 @@ namespace DoAn_WebNangCao.Controllers
                     noiDungDapAns.Add(Request[dapan_content_name]);
                     tinhChats.Add(order == true_answer_position ? true : false);
                 }
-                return Add_quiz_to_db(idLinhVuc, idLoaiCauHoi, noiDungCauHoi, noiDungDapAns, tinhChats);
+                Add_quiz_to_db(idLinhVuc, idLoaiCauHoi, noiDungCauHoi, noiDungDapAns, tinhChats);
             }
             else if(idLoaiCauHoi == Constant.ID_CAU_HOI_NHIEU_DAP_AN)
             {
@@ -96,7 +96,7 @@ namespace DoAn_WebNangCao.Controllers
                     noiDungDapAns.Add(Request[dapan_content_name]);
                     tinhChats.Add(Request[checkbox_name] != null ? true : false);
                 }
-                return Add_quiz_to_db(idLinhVuc, idLoaiCauHoi, noiDungCauHoi, noiDungDapAns, tinhChats);
+                Add_quiz_to_db(idLinhVuc, idLoaiCauHoi, noiDungCauHoi, noiDungDapAns, tinhChats);
             }
             else if(idLoaiCauHoi == Constant.ID_CAU_HOI_SAP_XEP_THEO_THU_TU)
             {
@@ -111,22 +111,54 @@ namespace DoAn_WebNangCao.Controllers
                     tinhChats.Add(true);
                     thuTus.Add(order);
                 }
-                return Add_quiz_to_db(idLinhVuc, idLoaiCauHoi, noiDungCauHoi, noiDungDapAns, tinhChats, thuTus);
-
+                Add_quiz_to_db(idLinhVuc, idLoaiCauHoi, noiDungCauHoi, noiDungDapAns, tinhChats, thuTus);
             }
-            return null;
+            else if(idLoaiCauHoi == Constant.ID_CAU_HOI_DIEN_VAO_CHO_TRONG)
+            {
+                string noiDungCauHoi = Request["fill-in-blank-quiz-content"];
+                int num_of_blanks = Int32.Parse(Request["fill-in-blank-quiz-num-of-blank"]);
+                int num_of_wrong_answers = Int32.Parse(Request["fill-in-blank-quiz-num-of-wrong-answer"]);
+                List<int> thuTus = new List<int>();
+                // Add blank answer
+                for(int idx = 0; idx < num_of_blanks; idx++)
+                {
+                    int order = idx + 1;
+                    string blank_answer_name = "blank-answer-content-" + order;
+                    noiDungDapAns.Add(Request[blank_answer_name]);
+                    tinhChats.Add(true);
+                    thuTus.Add(order);
+                }
+                CAUHOI cauhoi = Add_quiz_to_db(idLinhVuc, idLoaiCauHoi, noiDungCauHoi, noiDungDapAns, tinhChats, thuTus);
+                // Add wrong answer
+                noiDungDapAns.Clear();
+                for(int idx = 0; idx < num_of_wrong_answers; idx++)
+                {
+                    int order = idx + 1;
+                    string wrong_answer_name = "wrong-answer-content-" + order;
+                    noiDungDapAns.Add(Request[wrong_answer_name]);
+                }
+                Add_wrong_answer_of_fill_in_blank_quiz_to_db(cauhoi.IDCauHoi, noiDungDapAns);
+            }
+            return RedirectToAction("ManageQuiz", "Admin", new { idLinhVuc = idLinhVuc });
         }
 
-        private ActionResult Add_quiz_to_db(int idLinhVuc,
+        private void Add_wrong_answer_of_fill_in_blank_quiz_to_db(int idCauHoi,
+                                                                  List<string> noiDungDapAns)
+                                                                  
+        {
+            foreach(string noiDungDapAn in noiDungDapAns)
+            {
+                DAPAN dAPAN = new DAPAN();
+                dAPAN.Add_dap_an(noiDungDapAn, idCauHoi, false, null);
+            }
+        }
+
+        private CAUHOI Add_quiz_to_db(int idLinhVuc,
                                            int idLoaiCauHoi,
                                            string noiDungCauHoi,
                                            List<string> noiDungDapAns,
                                            List<bool> tinhChats)
         {
-            if (Session["Quyen"] != "Admin")
-            {
-                return RedirectToAction("Index", "HomePage");
-            }
             CAUHOI cauhoi = new CAUHOI();
             cauhoi = cauhoi.Add_cau_hoi(noiDungCauHoi, idLinhVuc, idLoaiCauHoi);        
             for(int idx = 0; idx < noiDungDapAns.Count; idx++)
@@ -137,20 +169,16 @@ namespace DoAn_WebNangCao.Controllers
                 DAPAN dapan = new DAPAN();
                 dapan.Add_dap_an(noiDungDapAn, idCauHoi, tinhChat, null);
             }
-            return RedirectToAction("ManageQuiz", "Admin", new { idLinhVuc = idLinhVuc });
+            return cauhoi;
         }
 
-        private ActionResult Add_quiz_to_db(int idLinhVuc,
+        private CAUHOI Add_quiz_to_db(int idLinhVuc,
                                            int idLoaiCauHoi,
                                            string noiDungCauHoi,
                                            List<string> noiDungDapAns,
                                            List<bool> tinhChats,
                                            List<int> thuTus)
         {
-            if (Session["Quyen"] != "Admin")
-            {
-                return RedirectToAction("Index", "HomePage");
-            }
             CAUHOI cauhoi = new CAUHOI();
             cauhoi = cauhoi.Add_cau_hoi(noiDungCauHoi, idLinhVuc, idLoaiCauHoi);
             for (int idx = 0; idx < noiDungDapAns.Count; idx++)
@@ -162,7 +190,7 @@ namespace DoAn_WebNangCao.Controllers
                 DAPAN dapan = new DAPAN();
                 dapan.Add_dap_an(noiDungDapAn, idCauHoi, tinhChat, thuTu);
             }
-            return RedirectToAction("ManageQuiz", "Admin", new { idLinhVuc = idLinhVuc });
+            return cauhoi;
         }
 
     }
--- DoAn_WebNangCao/Scripts/custom_script/add_fill_in_blank_quiz.js
+++ DoAn_WebNangCao/Scripts/custom_script/add_fill_in_blank_quiz.js
@@ -3,7 +3,6 @@
 $('#btn-add-blank').click(function (event) {
 	event.preventDefault();
 	var quiz_content = $quiz_content.val()
-	console.log(quiz_content)
 	quiz_content = quiz_content.concat(" {'blank'} ")
 	$quiz_content.val(quiz_content)
 	$quiz_content.focus()
@@ -13,19 +12,23 @@ $('#btn-add-blank').click(function (event) {
 $('#btn-add-fill-in-blank-quiz').click(function (e) {
 	e.preventDefault()
 	var order = ($('.wrong-answer-container').children().length) + 1
+	$('input[name=fill-in-blank-quiz-num-of-wrong-answer]').val(order)
 	var title = 'Đáp án ' + order
+	var wrong_answer_name = 'wrong-answer-content-' + order
 	var answer_row = '<div class="answer-row">' +
 		'<span>' + title + '</span>' +
-		'<input type="text" class="fill-in-blank-quiz-answer-content" placeholder="Nội dung đáp án"></div>'
+		'<input type="text" name=' + wrong_answer_name + ' class="fill-in-blank-quiz-answer-content" placeholder="Nội dung đáp án"></div>'
 	$('.wrong-answer-container', '.fill-in-blank-quiz').append(answer_row)
 })
 
 const Append_Blank_Answer = () => {
 	var order = ($('.blank-answer-container').children().length) + 1
+	$('input[name=fill-in-blank-quiz-num-of-blank]').val(order)
 	var title = 'Ô trống ' + order
+	var blank_answer_name = 'blank-answer-content-' + order
 	var answer_row = '<div class="answer-row">' +
 		'<span>' + title + '</span>' +
-		'<input type="text" class="fill-in-blank-quiz-answer-content" placeholder="Nội dung đáp án"></div>'
+		'<input type="text" name=' + blank_answer_name + ' class="fill-in-blank-quiz-answer-content" placeholder="Nội dung đáp án"></div>'
 	$('.blank-answer-container', '.fill-in-blank-quiz').append(answer_row)
 
 }
